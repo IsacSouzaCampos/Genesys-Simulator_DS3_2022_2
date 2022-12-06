cd4743940670e277a856f8517f138ca55902cd69
isacsouzacampos <isacsouzacampos@hotmail.com>
2022-12-05 21:38:21 -0300

    Iniciada implementação do modelo Defining Resource Capacity

--- source/applications/terminal/examples/smarts/Smart_DefiningResourceCapacity.cpp
+++ source/applications/terminal/examples/smarts/Smart_DefiningResourceCapacity.cpp
@@ -20,7 +20,8 @@
 
 // Model Components
 #include "../../../../plugins/components/Create.h"
-#include "../../../../plugins/components/DummyComponent.h"
+#include "../../../../plugins/components/Separate.h"
+#include "../../../../plugins/components/Process.h"
 #include "../../../../plugins/components/Dispose.h"
 
 Smart_DefiningResourceCapacity::Smart_DefiningResourceCapacity() {
@@ -34,16 +35,52 @@ int Smart_DefiningResourceCapacity::main(int argc, char** argv) {
 	Simulator* genesys = new Simulator();
 	this->setDefaultTraceHandlers(genesys->getTracer());
 	this->insertFakePluginsByHand(genesys);
-	// crete model
+	
+        
+        // crete model
 	Model* model = genesys->getModels()->newModel();
 	PluginManager* plugins = genesys->getPlugins();
-	Create* create1 = plugins->newInstance<Create>(model);
-	DummyComponent* dummy1 = plugins->newInstance<DummyComponent>(model);
-	Dispose* dispose1 = plugins->newInstance<Dispose>(model);
-	// connect model components to create a "workflow"
-	create1->getConnections()->insert(dummy1);
-	dummy1->getConnections()->insert(dispose1);
-	// set options, save and simulate
+	
+        Create* create1 = plugins->newInstance<Create>(model);
+        create1->setTimeUnit(Util::TimeUnit::minute);
+        create1->setEntitiesPerCreation(1);  // igual EntitiesPerArrival?
+        
+        Separate* separate1 = plugins->newInstance<Separate>(model);
+//        separate1->
+	
+	Process* process1 = plugins->newInstance<Process>(model);
+        process1->getSeizeRequests()->insert(new SeizableItem(plugins->newInstance<Resource>(model)));
+	process1->setQueueableItem(new QueueableItem(plugins->newInstance<Queue>(model)));
+        // ...
+        process1->setPriority(2);
+        process1->setDelayTimeUnit(Util::TimeUnit::minute);
+        process1->setDelayExpression("tria(0.5,5,10)");
+        process1->isReportStatistics();
+        
+        Process* process2 = plugins->newInstance<Process>(model);
+        process2->getSeizeRequests()->insert(new SeizableItem(plugins->newInstance<Resource>(model)));
+	process2->setQueueableItem(new QueueableItem(plugins->newInstance<Queue>(model)));
+        // ...
+        process2->setPriority(2);
+        process2->setDelayTimeUnit(Util::TimeUnit::minute);
+        process2->setDelayExpression("tria(0.5,5,10)");
+        process2->isReportStatistics();
+        
+        Dispose* dispose1 = plugins->newInstance<Dispose>(model);
+        Dispose* dispose2 = plugins->newInstance<Dispose>(model);
+	
+        
+        // connect model components to create a "workflow"
+	create1->getConnections()->insert(separate1);
+        
+        separate1->getConnections()->insert(process1);
+        separate1->getConnections()->insert(process2);
+        
+        process1->getConnections()->insert(dispose1);
+        process2->getConnections()->insert(dispose2);
+	
+        
+        // set options, save and simulate
 	model->getSimulation()->setReplicationLength(60, Util::TimeUnit::second);
 	model->save("./models/Smart_DefiningResourceCapacity.gen");
 	model->getSimulation()->start();
