83b0cb177b408f002aec03d8ce014891c8fb904c
Igor May Wensing <igormaywensing@gmail.com>
2022-12-16 20:00:17 -0300

    Added DefiningResourceCapacity

--- source/applications/terminal/examples/smarts/Smart_DefiningResourceCapacity.cpp
+++ source/applications/terminal/examples/smarts/Smart_DefiningResourceCapacity.cpp
@@ -20,7 +20,7 @@
 
 // Model Components
 #include "../../../../plugins/components/Create.h"
-#include "../../../../plugins/components/Separate.h"
+#include "../../../../plugins/components/Clone.h"
 #include "../../../../plugins/components/Process.h"
 #include "../../../../plugins/components/Dispose.h"
 
@@ -42,49 +42,79 @@ int Smart_DefiningResourceCapacity::main(int argc, char** argv) {
 	PluginManager* plugins = genesys->getPlugins();
 	
         Create* create = plugins->newInstance<Create>(model);
+        create->setTimeBetweenCreationsExpression("exp(3)");
         create->setTimeUnit(Util::TimeUnit::minute);
         create->setEntitiesPerCreation(1);  // igual EntitiesPerArrival?
         create->setFirstCreation(0);
+        //create->setMaxCreations(INFINITY);
         
-        Separate* separate1 = plugins->newInstance<Separate>(model);
-//        separate1->
-	
+        
+        //Resources
+        Resource* bookkeeper1 = plugins->newInstance<Resource>(model);
+        bookkeeper1->setCapacity(1);
+        //bookkeeper1->setCostBusyTimeUnit(0);
+        //bookkeeper1->setCostIdleTimeUnit(0);
+  
+        Resource* bookkeeper2 = plugins->newInstance<Resource>(model);
+        bookkeeper2->setCapacity(1);
+        //bookkeeper2->setCostBusyTimeUnit(0);
+        //bookkeeper2->setCostIdleTimeUnit(0);
+        
+        //Queues
+        Queue* queue1 = plugins->newInstance<Queue>(model);
+        //queue1->setOrderRule(1); // :ModelDataDefinition::OrderRule(1)
+        queue1->setReportStatistics(true);
+        
+        Queue* queue2 = plugins->newInstance<Queue>(model);
+        //queue2->setOrderRule(1);
+        queue2->setReportStatistics(true);
+        
+        //Clone
+        Clone* clone = plugins->newInstance<Clone>(model);
+        clone->setReportStatistics(false);
+
+        //Processes
 	Process* process1 = plugins->newInstance<Process>(model);
-        process1->getSeizeRequests()->insert(new SeizableItem(plugins->newInstance<Resource>(model)));
-	process1->setQueueableItem(new QueueableItem(plugins->newInstance<Queue>(model)));
-        // ...
+        process1->getSeizeRequests()->insert(new SeizableItem(bookkeeper1));
+	process1->setQueueableItem(new QueueableItem(queue1));
         process1->setPriority(2);
         process1->setDelayTimeUnit(Util::TimeUnit::minute);
         process1->setDelayExpression("tria(0.5,5,10)");
         process1->isReportStatistics();
         
         Process* process2 = plugins->newInstance<Process>(model);
-        process2->getSeizeRequests()->insert(new SeizableItem(plugins->newInstance<Resource>(model)));
-	process2->setQueueableItem(new QueueableItem(plugins->newInstance<Queue>(model)));
-        // ...
+        process2->getSeizeRequests()->insert(new SeizableItem(bookkeeper2));
+        process2->setQueueableItem(new QueueableItem(queue2));        // ...
         process2->setPriority(2);
         process2->setDelayTimeUnit(Util::TimeUnit::minute);
         process2->setDelayExpression("tria(0.5,5,10)");
         process2->isReportStatistics();
+        
         
         Dispose* dispose1 = plugins->newInstance<Dispose>(model);
         Dispose* dispose2 = plugins->newInstance<Dispose>(model);
 	
         
         // connect model components to create a "workflow"
-	create->getConnections()->insert(separate1);
+	create->getConnections()->insert(clone);
         
-        separate1->getConnections()->insert(process1);
-        separate1->getConnections()->insert(process2);
+        clone->getConnections()->insert(process1);
+        clone->getConnections()->insert(process2);
         
         process1->getConnections()->insert(dispose1);
         process2->getConnections()->insert(dispose2);
 	
         
         // set options, save and simulate
-	model->getSimulation()->setReplicationLength(60, Util::TimeUnit::second);
-	model->save("./models/Smart_DefiningResourceCapacity.gen");
-	model->getSimulation()->start();
+        ModelSimulation* sim = model->getSimulation();
+        
+        //sim->setTerminatingCondition("count(Dispose_1.CountNumberIn)>1000");
+        sim->setReplicationLength(480);//tem q variar
+        sim->setReplicationLengthTimeUnit(Util::TimeUnit::minute);
+        sim->setNumberOfReplications(300);
+        sim->setWarmUpPeriod(sim->getReplicationLength()*0.05); //5% de 480 = 24
+        sim->setWarmUpPeriodTimeUnit(Util::TimeUnit::minute);	model->save("./models/Smart_DefiningResourceCapacity.gen");
+	sim->start();
         
         for(int i = 0; i < 1e9; i++);
 	delete genesys;
